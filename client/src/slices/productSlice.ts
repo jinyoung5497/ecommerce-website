a30608700d35644fa5077fefdaa0ed758c310d38
jinyoung5497 <jinyoung5497@gmail.com>
2023-11-01 12:21:18 +1000

    server update v7

--- client/src/slices/productSlice.ts
+++ client/src/slices/productSlice.ts
@@ -135,7 +135,6 @@ export const productSlice = createSlice({
     },
     gallery1: (state, action: PayloadAction<string>) => {
       state.value.gallery1 = action.payload
-      console.log(state.value.gallery1)
     },
     gallery2: (state, action: PayloadAction<string>) => {
       state.value.gallery2 = action.payload
